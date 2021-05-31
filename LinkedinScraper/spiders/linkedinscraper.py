2c3d8e478d2ac26e81201b272f1d7cf72a328f11
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-31 13:37:09 +0530

    some changes

--- LinkedinScraper/spiders/linkedinscraper.py
+++ LinkedinScraper/spiders/linkedinscraper.py
@@ -20,11 +20,11 @@ wb.find_element_by_xpath('//a[text() = "Sign in"]').click()
 time.sleep(3)
 
 username = wb.find_element_by_xpath('//input[@id = "username"]')
-username.send_keys("jayparekh0408@gmail.com")
+username.send_keys("example@gmail.com")
 time.sleep(0.5)
 
 password = wb.find_element_by_xpath('//input[@id = "password"]')
-password.send_keys("Jayparelh@01")
+password.send_keys("xxxxxxxxxx")
 time.sleep(0.5)
 
 wb.find_element_by_xpath('//button[@type = "submit"]').click()
